ec7635db84968618e3bc63f67be69909398cf60e
Tim <you@example.com>
2018-09-09 12:04:55 +0200

    Added cookie basis

--- app/src/App.js
+++ app/src/App.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import Checkpoint from "./Components/Checkpoint";
 import {getPageData} from "./State/state";
+import { withCookies, Cookies } from 'react-cookie';
 
 class App extends Component {
+   
+    constructor(props) {
+      super(props)
+   
+      const { cookies } = props;
+      
+      const stationdata = {}
+      try {
+        const stationdata = JSON.parse(cookies.get('QRnonerka'))
+      }
+      catch(e) {
+      }
 
+      this.state = {
+        stations: stationdata,
+        data: getPageData(1)
+      }
+
+      cookies.set('QRnonerka', JSON.stringify({}))
+    }
 
   render() {
-    const pageProps = getPageData(1)
-
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">QRnonerka</h1>
         </header>
-        <Checkpoint {...pageProps}/>
+        <Checkpoint {...this.state.data}/>
       </div>
     );
   }
 }
 
-export default App;
+export default withCookies(App);
--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { CookiesProvider } from 'react-cookie';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<CookiesProvider>
+		<App />
+	</CookiesProvider>
+, document.getElementById('root'));
