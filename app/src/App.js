ce887da47f2bfccaa44fab44852b212f0d632df0
Tim <you@example.com>
2018-09-09 14:14:41 +0200

    Added timestamping

--- app/src/App.js
+++ app/src/App.js
@@ -6,30 +6,36 @@ import { withCookies, Cookies } from 'react-cookie';
 
 class App extends Component {
    
-    constructor(props) {
-      super(props)
-   
-      const { cookies } = props;
-      
-      const stationdata = {}
-      try {
-        const stationdata = JSON.parse(cookies.get('QRnonerka'))
-      }
-      catch(e) {
-      }
-
-      this.state = {
-        stations: stationdata,
-        data: getPageData(1)
-      }
-
-      cookies.set('QRnonerka', JSON.stringify({}))
+  constructor(props) {
+    super(props)
+ 
+    const { cookies } = props;
+    const stationdata = {}
+    try {
+      const stationdata = JSON.parse(cookies.get('QRnonerka'))
+      console.log(stationdata)
     }
+    catch(e) {
+    }
+
+    this.state = {
+      stations: stationdata,
+      data: getPageData(1)
+    }
+
+    cookies.set('QRnonerka', JSON.stringify(
+      Object.assign(stationdata, 
+        {'1': (new Date()).getTime()}
+        )
+      )
+    )
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        {JSON.stringify(this.state.stations)}
           <h1 className="App-title">QRnonerka</h1>
         </header>
         <Checkpoint {...this.state.data}/>
