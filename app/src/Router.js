ccc1148eec45ed14bdf348664bcb7359eb5c9b42
Tim <you@example.com>
2018-09-09 21:00:31 +0200

    Heroku magic

--- app/src/Router.js
+++ app/src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import App from './App.js'
 import { withCookies, Cookies } from 'react-cookie'
 
