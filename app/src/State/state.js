8faed5308222ed2e664943ffa29dd922e4e5e3d8
Maximilian Eissler <maxi@Maximilians-MacBook-Pro.local>
2018-09-11 12:47:15 +0300

    added new pictures and new build

--- app/src/State/state.js
+++ app/src/State/state.js
@@ -14,7 +14,7 @@ const pageData = [
   },
   {
     id: 18819,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_7949.jpg',
     title: 'Defender',
     text: 'The Western part of the Kanonerskiy island, facing the Neva Bay, has never been fortified or protected. Here it is a shallow area, which is part of the Nevsky bar.  On this shore there was a battery with a powder, ammunition depot. In the first half of the XVIII century the island was controlled by the Admiralty Board. The Island transformed into an educational center. Naval gunners started studding the firing skill. The place got a new name `Kanoner-skiy Island` in the French manner. That means `Cannoner Island`.Look into the distance. Breathe and dream. Imagine the thunder of the powerful cannons. What the emotions does the place now?',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -22,7 +22,7 @@ const pageData = [
   },
   {
     id: 21810,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_8247.jpg',
     title: 'Lollipop',
     text: 'In the middle of the beach you see the object that has never been used as intended. There is a pipe. It is great and alone. It was built in 1980 as a part of an unfinished thermal power plant. It is a reminder about human ambition and an example of urban style. We suggest you to think on the item “How can we name the pipe? Do we have to save the pipe? How can we use the pipe?”',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -30,7 +30,7 @@ const pageData = [
   },
   {
     id: 28983,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/GateToEurope.jpg',
     title: 'A Gate to Europe',
     text: 'The shape of the island has changed many times both under the influence of natural phenomenons and as a result of human-made processes. In the end of 19th century, the configuration of the island has changed significantly. In particular, the territory in the North and the South became longer, a river Batareyka was filled and a part of the Kanonevsky territory went to the Gutuevsky Island. Now you stay in front of the Sea canal that was laid in 1875-1885. The sea canal is an artificial waterway in the Neva Bay extending from the estuary of the Bolshaya Neva river to the Kotlin island. It was designed to allow the ships to get to the seaport of St. Petersburg. Just imagine! It is almost 30 kilometers of waterway with greatest depth in 14 meters. It is deserved to be named. We allow you to discuss on this topic.',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -38,7 +38,7 @@ const pageData = [
   },
   {
     id: 4353,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_8198.jpg',
     title: 'The Round Island',
     text: 'On the opposite site of the canal you see another island. It is connected with the Kanonerskiy island with an underwater tonal that was opened in 1983. Its name is the Gutuevsky Island. The name comes from the owner of the island. His name is Konon Gutuev. He belong to the merchant class. The island is famous by its sea port and Church Of The Epiphany. Have you ever been to Gutuevsky before?',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -46,7 +46,7 @@ const pageData = [
   },
   {
     id: 48104,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/WhiteIsland.png',
     title: 'White Island',
     text: 'Wastewater treatment plant of St. Petersburg is located on an artificial island. It is the oldest system of treatment facilities in the city, accepts domestic, industrial and surface waste water. The first part of the station began to work in 1978 and now it works observing the standards of the Helsinki Commission.',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -54,7 +54,7 @@ const pageData = [
   },
   {
     id: 56754,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_8266.jpg',
     title: 'Kanonersky Shipyard',
     text: 'Kanonersky Shipyard was built in the 1880s simultaneously with Morskoy canal. In 1930 the dock with 2 sections appeared. It can carry up to 4 tons of load.During the war the shipyard was rebuilt for military needs. On the territory of the shipyard there is a monument to workers who fell during the WWII.',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -62,7 +62,7 @@ const pageData = [
   },
   {
     id: 48695,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_8261.jpg',
     title: 'OK-Line',
     text: 'There is the so-called “OK-line”. On the left side of this building you can see abandoned flats, but on the right side there is residential apartments that are used even to this day.Do you think, is it really OK to live here, or it is too noisy?',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -70,7 +70,7 @@ const pageData = [
   },
   {
     id: 43708,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_7868.jpg',
     title: 'H. 17',
     text: 'Look at this abandoned building. It is too noisy to live here. Despite this you can use this building somehow. For example, you can make a post-apocalyptic movie or commemorate your cat. What are your suggestions how to use this place?',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -78,7 +78,7 @@ const pageData = [
   },
   {
     id: 62187,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_7923.jpg',
     title: 'The Bridge',
     text: 'Now you are standing almost below the huge Western High-Speed Diameter of St. Petersburg (ZSD). First some facts: The motorway was built between 2008 - 2016 and cost 210 billion rubles. It is possible for car drivers to cross the city in less than 20 minutes when using this 47-kilometres road.  But there are also some problems at this place, caused by the motorway`s noise and dust. To reduce the noise, authorities already built a second floor and a noise-protection wall on the motorway. But noise measurements still show significant violations of the allowed amount of noise at residential places. In addition, there are sometimes things falling down from the road. By now, a few buildings are abandoned because living there is not healthy anymore. To care about the residents of Kanoniersky Island, there is currently the suggestion of a new protection against the noise. Do you like this idea? What do you think about living near the bridge?',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
@@ -86,7 +86,7 @@ const pageData = [
   },
   {
     id: 8980,
-    imageUrl: './resources/DSC_7859.jpg',
+    imageUrl: './resources/DSC_8211.jpg',
     title: 'Everlasting Memory',
     text: 'On the night of 10 to 11 September, 1941 Kanonersky Island was attacked by nazi. There were dropped thousands of incendiary and high-explosive bombs. The island was burning, but people stood firm!\n' + 'Let us honor their memory with a minute’s silence',
     hint: 'This is a hint. To find the next spot, turn around three times and spit on the floor and so on ...',
